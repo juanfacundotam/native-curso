fe640a8456665efd54928c14d22cfa7a4839a12f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-13 00:37:44 -0300

    Update NavigationDrawer.js

--- src/navigation/NavigationDrawer.js
+++ src/navigation/NavigationDrawer.js
@@ -1,14 +1,18 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import HomeView from '../views/HomeView'
+import SettingsView from '../views/SettingsView'
 
-export default class NavigationDrawer extends Component {
-  render() {
-    return (
-      <View>
-        <Text>NavigationDrawer</Text>
-      </View>
-    )
-  }
+const Drawer = createDrawerNavigator();
+
+export default function NavigationDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeView}/>
+      <Drawer.Screen name="Settings" component={SettingsView}/>
+    </Drawer.Navigator>
+  )
 }
 
 // import { View, Text } from 'react-native'
