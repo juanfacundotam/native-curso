70e8236c31a71e1437c9db72b6e3e6ea47f751cf
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-13 00:16:02 -0300

    Update SettingsView.js

--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-export default function SettingsView() {
+export default function SettingsView(props) {
+  const {navigation} = props
+  const goToPage = (pageName) => {
+    navigation.navigate(pageName)
+  }
   return (
     <View>
       <Text>SettingsScreen</Text>
       <Text>SettingsScreen</Text>
       <Text>SettingsScreen</Text>
       <Text>SettingsScreen</Text>
+      <Button onPress={() => goToPage("Home")} title='Ir a Home'/>
     </View>
   )
 }
